ea14549b884f8918ec2cfe73e1d443904bd260ca
강창래 <crkang@covision.co.kr>
2024-11-19 12:41:36 +0900

    :art: add recoil state in MemoList

--- washswattest/screens/MemoListPage.js
+++ washswattest/screens/MemoListPage.js
@@ -1,10 +1,24 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {memosState} from '../atoms/memoState';
+import {useRecoilState} from 'recoil';
 
 const MemoListPage = () => {
+  const [memos, setMemos] = useRecoilState(memosState);
+
   return (
     <View>
-      <Text>MemoListPage</Text>
+      <FlatList
+        data={memos}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <View>
+            <Text>{item.title}</Text>
+            <Text>{item.description}</Text>
+            <Text>{item.updatedAt}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
