a9903656f4152e4580fd7fad9dfb118a3ded6deb
강창래 <crkang@covision.co.kr>
2024-11-20 18:21:55 +0900

    :sparkles: add go back function

--- washswattest/src/components/Header.js
+++ washswattest/src/components/Header.js
@@ -1,17 +1,30 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
 import {memoTitleState, memosState} from '../atoms/memoState';
 
 import {useRecoilValue} from 'recoil';
+import {useNavigation} from '@react-navigation/native';
+import {LeftArrowIcon} from '../assets/Icon';
+
 const Header = ({title, memoCnt}) => {
   const memoTitle = useRecoilValue(memoTitleState); // 제목 상태 읽기
   const memos = useRecoilValue(memosState); // 전체 메모 상태 읽기
+  const navigation = useNavigation();
+
   return (
     <View style={styles.header}>
+      {!!!title && (
+        <TouchableOpacity
+          style={styles.iconContainer}
+          onPress={() => navigation.goBack()}>
+          <LeftArrowIcon width={24} height={24} fill="#fff" />
+        </TouchableOpacity>
+      )}
       <Text style={styles.headerText}>
         {title ? title : memoTitle}
         {memoCnt ? `(${memos.length})` : null}
       </Text>
+      {!!!title && <View style={styles.rightSpacer} />}
     </View>
   );
 };
@@ -20,13 +33,23 @@ export default Header;
 
 const styles = StyleSheet.create({
   header: {
-    backgroundColor: '#000', // 배경 검은색
-    padding: 15, // 패딩 추가
-    alignItems: 'center', // 텍스트를 수평 가운데 정렬
+    backgroundColor: '#000',
+    padding: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  iconContainer: {
+    position: 'absolute',
+    left: 15,
   },
   headerText: {
-    color: '#fff', // 글자 흰색
-    fontSize: 18, // 글자 크기
-    fontWeight: 'bold', // 글자 굵게
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  rightSpacer: {
+    width: 24, // 오른쪽 공간 확보용
   },
 });
